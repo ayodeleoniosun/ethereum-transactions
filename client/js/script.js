9908cf2e80f290d233743ab32ffa5fe8855ab2f1
Ayodele Oniosun <ayodeleoniosun63@gmail.com>
2024-06-06 09:18:48 +0100

    feat: add client to connect to socket

---
+++ client/js/script.js
@@ -0,0 +1,15 @@
+let messages = document.getElementById('messages');
+
+const socket = io('http://localhost:3000');
+io('http://localhost:3000/admin');
+
+socket.on('connect', () => {
+    displayMessage("You connected with id " + socket.id);
+});
+
+function displayMessage(message) {
+    const item = document.createElement('li');
+    item.textContent = message;
+    messages.appendChild(item);
+    window.scrollTo(0, document.body.scrollHeight);
+}
